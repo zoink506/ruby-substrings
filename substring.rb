6a71dfe3eb3a95bb79a9d0d5c890aedcf1eff3b7
zoink506 <benhubble03@gmail.com>
2022-12-10 15:42:28 +1100

    Detect how many times words appear in string

--- substring.rb
+++ substring.rb
@@ -1,11 +1,30 @@
 def getInput()
   puts "Enter the string to search: "
   userInput = gets.chomp
-  substrings(userInput)
-end 
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  substrings(userInput, dictionary)
+end
 
-def substrings(string)
-  puts string
+def substrings(input_string, dictionary)
+  #puts "dictionary: #{dictionary}"
+  #puts "string: #{input_string}"
+
+  # Loop through each string in dictionary
+  # Compare dictionary string to the input string
+  # If the input string contains the dictionary string, add it to the hash
+
+  string_array = input_string.split(" ")
+  output_hash = Hash.new(0)
+
+  string_array.each do |input_word|
+    dictionary.each do |dictionary_word|
+      if input_word.include? dictionary_word
+        output_hash[dictionary_word] += 1
+      end 
+    end 
+  end 
+
+  p output_hash
 end 
 
 getInput()
